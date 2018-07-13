cb3ee4700d87851384d1c3424434c6befb6b98c1
Karolina Siwkowska <trallala00@gmail.com>
2018-07-13 16:44:46 +0200

    Modify game.js,use js beautify tool

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const cardsColor = ["red","red", "yellow","yellow", "blue","blue","brown","brown","gray","gray","cadetblue","cadetblue","violet","violet","lightgreen","lightgreen","green","green"];
+const cardsColor = ["red", "red", "yellow", "yellow", "blue", "blue", "brown", "brown", "gray", "gray", "cadetblue", "cadetblue", "violet", "violet", "lightgreen", "lightgreen", "green", "green"];
 //pobieramy nasze wszystkie divy
 let cards = document.querySelectorAll("div");
 //w konsoli wyswietli nam sie lista ktora musimy przerobic na tablice 
@@ -14,67 +14,68 @@ cards = [...cards];
 const startTime = new Date().getTime();
 let activeCard = "";
 const activeCards = [];
-
-const gamePairs = cards.lenght/2;
+const gameLength = cards.length / 2;
+//const gamePairs = cards.lenght/2;
 let gameResult = 0;
 
-const clickCard = function(){
-//console.log("klikanie dziala");
-activeCard = this;
-activeCard.classList.remove("hidden");
+const clickCard = function() {
+    //console.log("klikanie dziala");
+    activeCard = this;
+    activeCard.classList.remove("hidden");
 
-if(activeCards.length === 0){
-    activeCards[0] = activeCard;
-    return;
-}
-else {
-    cards.forEach(card => card.removeEventListener("click",clickCard))
-    activeCards[1]= activeCard;
-    setTimeout(function(){  
-    if(activeCards[0].className === activeCards[1].className){
-       console.log("Wygrana")
-       activeCards.forEach(card => card.classList.add("off"))
-    
-       gameResult++;
-       if ( gameResult == gamePairs){
-           console.log("WYGRANA")
-       }
+    if (activeCards.length === 0) {
+        activeCards[0] = activeCard;
+        return;
+    } else {
+        cards.forEach(card => card.removeEventListener("click", clickCard))
+        activeCards[1] = activeCard;
+        setTimeout(function() {
+            if (activeCards[0].className === activeCards[1].className) {
+                console.log("Wygrana")
+                activeCards.forEach(card => card.classList.add("off"))
+                gameResult++;
+                cards = cards.filter(card => !card.classList.contains("off"));
+                if (gameResult == gameLength) {
+                    const endTime = new Date().getTime();
+                    const gameTime = (endTime - startTime) / 1000
+                    alert(`Udało się! Twój wynik to: ${gameTime} sekund`)
+                    location.reload();
+                }
+            } else {
+                console.log("Przegrana")
+                activeCards.forEach(card => card.classList.add("hidden"))
+            }
+
+            activeCard = "";
+            activeCards.length = 0;
+            cards.forEach(card => card.addEventListener("click", clickCard))
+        }, 1000)
     }
-    else{
-        console.log("Przegrana")
-        activeCards.forEach(card => card.classList.add("hidden"))
-    }
-    
-    activeCard = "";
-    activeCards.length = 0;
-    cards.forEach(card => card.addEventListener("click", clickCard))
-},1000)
-}
-}
+};
 
 
 
 
 // nadajemy kolory naszej tablice ktore beda losowe,musimy wylosowac kolory
-const init = function () {
+const init = function() {
     //cards.forEach(function (card) zapis ES5
     cards.forEach(card => {
-    //losujemy pozycje z tablicy
-        const position = Math.floor(Math.random()*
-        cardsColor.length);
+        //losujemy pozycje z tablicy
+        const position = Math.floor(Math.random() *
+            cardsColor.length);
         //przypisanie wylosowanej klasy do elementu
         card.classList.add(cardsColor[position]);
         //musimy usnac element ktory wylosowalismy metoda splice
         cardsColor.splice(position, 1);
-   })
-   //po czasie chcemy ukryc elementy na 2sek
-   setTimeout(function(){
-       cards.forEach(card => {
-           card.classList.add("hidden")
-           //ustawiamy nasluchiwanie na klikniecie zeby mozna bylo kliknac w element
-           card.addEventListener("click",clickCard)
-       })
-   },2000)
+    })
+    //po czasie chcemy ukryc elementy na 2sek
+    setTimeout(function() {
+        cards.forEach(card => {
+            card.classList.add("hidden")
+            //ustawiamy nasluchiwanie na klikniecie zeby mozna bylo kliknac w element
+            card.addEventListener("click", clickCard)
+        })
+    }, 2000)
 }
 
 init()
